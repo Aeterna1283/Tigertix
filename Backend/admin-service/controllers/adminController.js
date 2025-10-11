056fccd97b65facdc52b34be0592130a88c09826
Aeterna1283 <rb9362123@gmail.com>
2025-10-11 16:21:30 -0400

    Update adminController.js

--- Backend/admin-service/controllers/adminController.js
+++ Backend/admin-service/controllers/adminController.js
@@ -1,10 +1,10 @@
-const modelsFunctions = require('../models/adminModel');
+const adminModel = require('../models/adminModel');
 
 async function createEvent(req, res){
 
     try{
         const {event_name, event_date, event_tickets, event_location } = req.body;
-        const newEvent = await modelsFunctions.createEvent(event_name, event_date, event_tickets, event_location);    
+        const newEvent = await adminModel.createEvent(event_name, event_date, event_tickets, event_location);    
         const newTicket = await adminModel.createTicket(newEvent.event_id, event_tickets, ticket_price, ticket_type);
         res.status(201).json({event: newEvent, ticket: newTicket});
     } catch (err) {
