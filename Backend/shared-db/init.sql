4b9053e3f0489522bdc503bac2d5dd4e0c89af60
Aeterna1283 <rb9362123@gmail.com>
2025-10-11 11:23:20 -0400

    created the sql database

---
+++ Backend/shared-db/init.sql
@@ -0,0 +1,27 @@
+--for toggling on foreign keys, which ticket needs to access
+PRAGMA foreign_keys = ON;
+
+--if a table exists then drop it
+DROP TABLE IF EXISTS Ticket;
+DROP TABLE IF EXISTS EVENT;
+
+-- event table
+CREATE TABLE Event (
+    event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event_name TEXT NOT NULL,
+    event_date TEXT NOT NULL,
+    event_tickets INTEGER NOT NULL,
+    event_location TEXT NOT NULL
+
+);
+
+--ticket id
+CREATE TABLE Ticket (
+ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
+event_id INTEGER NOT NULL, --the foreign key that accesses the event table
+ticket_price REAL,
+ticket_type TEXT,
+ticket_availability BOOLEAN NOT NULL, -- no idea if we need this but i would assume you would want to know if tickets are available
+FOREIGN KEY(event_id) REFERENCES Event(event_id)
+
+);
